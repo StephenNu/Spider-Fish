06f71c8e4acdf73e918e9cb55ed4eefc3e9d91f3
Stephen N <nuskos@windsor.ca>
2011-10-30 16:24:16 -0400

    Updated the circle to have a min and max size, and made it so bullets change into enemies when they leave the circle.
    Signed-off-by: Stephen N <nuskos@windsor.ca>

--- src/Bullet.h
+++ src/Bullet.h
@@ -8,17 +8,23 @@
 #include "SpiderFish.h"
 #include "Sprite.h"
 #include "PhysicalEntity.h"
-
+#include "ShipHalo.h"
 
 class Bullet : public PhysicalEntity
 {
   public:
-    Bullet( Game* owner, SDL_Surface* image, int x, int y, int vx, int vy );
+	  // Added a new constructor that passes a ShipHalo reference this should only be called when it is a player ship. Stephen
+    Bullet( Game* owner, SDL_Surface* image, int x, int y, int vx, int vy, ShipHalo* halo);
+    Bullet( Game* owner, SDL_Surface* image, int x, int y, int vx, int vy);
     virtual ~Bullet();
     virtual void draw( SDL_Surface* );
     virtual void update( int msTimeStep );
     virtual void handleEvent(SDL_Event* event);
   protected:
+	  // Added five variables one to pass the halo down to the bullet, and two booleans to check if a player shoot it and if they did is it inside the circle. Stephen
+	  ShipHalo* _halo;
+	  bool isPlayer;
+	  bool isInside;
   private:
 };
 
--- src/Weapon.cpp
+++ src/Weapon.cpp
@@ -1,13 +1,23 @@
 #include "Weapon.h"
-
-Weapon::Weapon( Game* owner, Ship* body ):
+// Added ShipHalo* as a parameter Should only be used for a player's ship weapon. Stephen
+Weapon::Weapon( Game* owner, Ship* body, ShipHalo* halo ):
+  GameObject( owner ),
+  _body( body ),
+  _lastShot( 0 ),
+  _firingSpeed( 0 ),
+  _triggerPulled( false ),
+  _halo( halo )
+{
+  //ctor
+}
+Weapon::Weapon(Game* owner, Ship* body):
   GameObject( owner ),
   _body( body ),
   _lastShot( 0 ),
   _firingSpeed( 0 ),
   _triggerPulled( false )
 {
-  //ctor
+	// empty
 }
 
 Weapon::~Weapon()
